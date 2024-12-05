915e141dcb015d3dfb43d8004dd914bb7108e9d1
Miss Trupti <truptiwagaskar4937@gmail.com>
2024-12-05 10:08:08 -0500

    Create function addTask

--- script.js
+++ script.js
@@ -9,4 +9,12 @@ document.getElementById('todo-form').addEventListener('submit', function (e) {
       input.value = '';
     }
   });
+
+  function addTask(task) {
+    const list = document.getElementById('todo-list');
   
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+  
+    const text = document.createElement('span');
+    text.textContent = task;
